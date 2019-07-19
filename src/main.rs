e02091c6130df8f63002b4f31bd0c8e65ef31dc7
Duane Johnson <duane.johnson@gmail.com>
2019-07-19 15:43:24 -0600

    Use main.rs; create ngrams

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+#[allow(unused_imports)]
+
+#[macro_use]
+extern crate gumdrop;
+
+use gumdrop::Options;
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::str;
+
+mod normalize;
+
+#[derive(Debug, Options)]
+struct GramsOptions {
+    #[options(free, help = "text files to read as n-grams")]
+    files: Vec<String>,
+
+    #[options(short = "h", help = "show help message")]
+    help: bool,
+
+    #[options(short = "n", help = "number of words per n-gram")]
+    number: Option<i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: GramsOptions = GramsOptions::parse_args_default_or_exit();
+
+    let mut buffer: Vec<u8> = Vec::new();
+
+    let number: usize = match opts.number {
+        Some(n) => n as usize,
+        None => 1
+    };
+    if opts.files.len() == 0 {
+        // Read from STDIN
+        io::stdin().read_to_end(&mut buffer).unwrap();
+        let result = normalize::normalize_ascii(&buffer);
+
+        let stdout = io::stdout();
+        let mut out_handle = stdout.lock();
+        for sentence in result.split(|c| c == &b'.') {
+            if sentence.len() > 0 {
+                let words: Vec<&[u8]> = sentence.split(|c| c == &b' ').collect();
+                for ngram in words.windows(number) {
+                    let mut first_word_written = false;
+                    for word in ngram {
+                        if first_word_written {
+                            out_handle.write_all(&[b' ']).unwrap();
+                        } else {
+                            first_word_written = true;
+                        }
+                        out_handle.write_all(word).unwrap();
+                    }
+                    out_handle.write_all(&[b'\n']).unwrap();
+                }
+            }
+        }
+    }
 }
---
+++ src/normalize.rs
@@ -0,0 +1,78 @@
+use std::fmt;
+
+#[derive(Clone, Copy)]
+enum CharType {
+    Letter(u8),
+    WordSep,
+    SentenceSep,
+    Join,
+    Skip,
+}
+
+impl fmt::Debug for CharType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CharType::Letter(a) => write!(f, "Letter({} ({}))", *a as char, a),
+            CharType::WordSep => write!(f, "WordSep"),
+            CharType::SentenceSep => write!(f, "SentenceSep"),
+            CharType::Join => write!(f, "Join"),
+            CharType::Skip => write!(f, "Skip"),
+        }
+    }
+}
+
+pub fn normalize_ascii(text: &Vec<u8>) -> Vec<u8> {
+    let mut write_buffer = Vec::with_capacity(text.len());
+    let mut t1: CharType = CharType::Skip;
+    let mut t2: CharType;
+    for this_char in text.iter() {
+        t2 = match this_char {
+            b'A'...b'Z' => CharType::Letter(*this_char + 32),
+            b'a'...b'z' => CharType::Letter(*this_char),
+            b' ' | b'\t' | b'\n' | b'"' | b'(' | b')' => CharType::WordSep,
+            b'!' | b'?' | b'.' => CharType::SentenceSep,
+            b'-' => CharType::Join,
+            _ => CharType::Skip,
+        };
+
+        // println!(
+        //     "{} ({}) {:?} {:?}",
+        //     char::from(*this_char),
+        //     this_char,
+        //     t1,
+        //     t2
+        // );
+        match (t1, t2) {
+            (CharType::Skip, _) => {
+                t1 = t2;
+            }
+            (CharType::Letter(a), _) => {
+                t1 = t2;
+                write_buffer.push(a);
+            }
+            (CharType::WordSep, CharType::Letter(_)) => {
+                t1 = t2;
+                write_buffer.push(b' ');
+            }
+            (CharType::WordSep, CharType::SentenceSep) => {
+                t1 = CharType::SentenceSep;
+            }
+            (CharType::SentenceSep, CharType::Letter(_)) => {
+                t1 = t2;
+                write_buffer.push(b'.');
+            }
+            (CharType::Join, CharType::Letter(_)) => {
+                t1 = t2;
+            }
+            (_, _) => (),
+        }
+    }
+
+    match t1 {
+        CharType::Letter(a) => write_buffer.push(a),
+        CharType::SentenceSep => write_buffer.push(b'.'),
+        _ => (),
+    }
+
+    return write_buffer;
+}

3f6b88906d0e05d707b17bd0c03db7e3eec1005b
Duane Johnson <duane.johnson@gmail.com>
2019-07-19 14:23:37 -0600

    in-place normalize working

--- src/bin/grams.rs
+++ src/bin/grams.rs
@@ -1,11 +1,14 @@
 #[allow(unused_imports)]
 #[macro_use]
 extern crate gumdrop;
+// #[macro_use] extern crate log;
 
 use gumdrop::Options;
 use std::io;
 use std::io::Read;
 use std::str;
+
+mod normalize;
 
 #[derive(Debug, Options)]
 struct GramsOptions {
@@ -27,9 +30,9 @@ fn main() {
     if opts.files.len() == 0 {
         // Read from STDIN
         io::stdin().read_to_end(&mut buffer).unwrap();
-        normalize_ascii(&mut buffer);
+        let result = normalize::normalize_ascii(&buffer);
 
-        let s = match str::from_utf8(&buffer) {
+        let s = match str::from_utf8(&result) {
             Ok(v) => v,
             Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
         };
@@ -37,34 +40,4 @@ fn main() {
     }
 
     // println!("{:#?}", opts);
-}
-
-fn normalize_ascii(text: &mut Vec<u8>) {
-    // let mut norm = String::with_capacity(text.len());
-    // let mut write_char: u8;
-    // let iter = &text.into_iter();
-    // let iter = &text.iter_mut();
-    // while let Some(&mut this_char) = iter.next() {
-    for this_char in text.iter_mut() {
-        let intermediate_char: u8 = match this_char {
-            b'A'...b'Z' => *this_char + 32,
-            b' ' | b'(' | b')' => b' ',
-            b'!' | b'?' => b'.',
-            _ => *this_char,
-        };
-
-        *this_char = intermediate_char;
-
-        // if next_char == '-' {
-        //     join_lines = true;
-        // } else if join_lines && next_char == ' ' {
-        //     // ignore whitespace after a dash (i.e. including newlines, which is the
-        //     // most common case because words that are broken by syllables are dashed)
-        // } else if c == '.' && !just_added_period {
-
-        // }
-
-        // norm.push(c.to_ascii_lowercase());
-    }
-    // return norm;
 }
